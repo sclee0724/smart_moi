609e36a1e50ea0517267cc37d72878a21f2ab99c
sclee0724 <sclee0724@gmail.com>
2022-01-07 09:39:28 +0900

    edit1

--- smart_moi.ino
+++ smart_moi.ino
@@ -1,24 +1,26 @@
-#include <Survo.h>
+#include <ESP32Servo.h>
 
 int touch_sensor = D2;
-int servoPin = D5
+static const int servoPin = D5;
 Servo servo;
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(115200);
-  servo.attach(servoPin)
-  pinMode(touch_sensor, INPUT)
+  servo.attach(servoPin);
+  servo.write(50);
+  pinMode(touch_sensor, INPUT);
 }
 
 void loop() {
-  int touch_sensor_result = digitalRead(touch_sensor)
-
-  servo.write(0)
-  Serial.println(touch_sensor_result)
+  int touch_sensor_result = digitalRead(touch_sensor);
+  
   
   if(touch_sensor_result == 1){
-    servo.write(40)
-    delay(2000)
+    servo.write(0);
+    delay(2000);
   }
+   else {
+    servo.write(50);
+   }
+  
 }
